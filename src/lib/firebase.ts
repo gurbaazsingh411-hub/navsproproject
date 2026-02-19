59b3dd51b8132b8bc89ce3c765e1639008c46da4
User Name <user@example.com>
2026-02-19 23:20:21 +0530

    feat: initialize Firebase application and export authentication service.

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCltmHJp3I09anZSp9qZjUg4q87J6Jhds0",
-    authDomain: "navspro-690f5.firebaseapp.com",
-    projectId: "navspro-690f5",
-    storageBucket: "navspro-690f5.firebasestorage.app",
-    messagingSenderId: "20856571168",
-    appId: "1:20856571168:web:72cc864f296414c87dc345",
-    measurementId: "G-XCLQBMFRVH"
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
+    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
